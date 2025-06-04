edbd270f129480178515d755c7e6504bd04c9349
Arthur Maciel <arthur6325@gmail.com>
2025-06-04 14:52:20 -0300

    Agendando tarefa

--- scripts/rodar_listar.bat
+++ scripts/rodar_listar.bat
@@ -1,4 +1,3 @@
 @echo off
-REM Rodar o script Python com ambiente virtual
-
 "C:\Users\Orçamento\OneDrive - GRUPO RETEC\02. Engenharia\Dep. Orçamentos\POWERBI\AUTOMACAO RD\autord\Scripts\python.exe" "C:\Users\Orçamento\OneDrive - GRUPO RETEC\02. Engenharia\Dep. Orçamentos\POWERBI\AUTOMACAO RD\scripts\listar.py"
+pause
