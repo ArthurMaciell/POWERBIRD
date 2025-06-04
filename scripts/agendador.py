8a7a2a9d8686875521b725ec5f8269c48cbc75bc
Arthur Maciel <arthur6325@gmail.com>
2025-06-04 14:22:37 -0300

    Agendador

---
+++ scripts/agendador.py
@@ -0,0 +1,19 @@
+import schedule
+import time as tm
+from datetime import time
+from schedule import repeat, every
+import subprocess
+
+def listar():
+    subprocess.run([
+        "C:\\Users\\Orçamento\\OneDrive - GRUPO RETEC\\02. Engenharia\\Dep. Orçamentos\\POWERBI\\AUTOMACAO RD\\autord\\Scripts\\python.exe",
+        "C:\\Users\\Orçamento\\OneDrive - GRUPO RETEC\\02. Engenharia\\Dep. Orçamentos\\POWERBI\\AUTOMACAO RD\\scripts\\listar.py"
+    ], check=True)
+
+schedule.every().day.at("14:04").do(listar)
+#schedule.every().hour.at(":56").do(listar)
+
+while True:
+    schedule.run_pending()
+    tm.sleep(1)
+    
