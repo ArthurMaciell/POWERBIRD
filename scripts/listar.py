cc67f22d1e1b11eef436d42725acc74ef784577a
Arthur Maciel <arthur6325@gmail.com>
2025-08-05 18:03:01 -0300

    atualização

--- scripts/listar.py
+++ scripts/listar.py
@@ -85,7 +85,7 @@ df_expanded = pd.concat([df_total, df_custom], axis=1)
 
 
 # Salvar
-caminho = r"C:\\Users\\Orçamento\\OneDrive - GRUPO RETEC\\02. Engenharia\\Dep. Orçamentos\\POWERBI\\AUTOMACAO RD\\data\\negociacoes_2025.xlsx"
+caminho = r"C:\Users\Orçamento\ONE DRIVE ORCAMENTO\OneDrive - GRUPO RETEC\02. Engenharia\Dep. Orçamentos\POWERBI\AUTOMACAO RD\data\negociacoes_2025.xlsx"
 df_expanded.to_excel(caminho, index=False)
 
 # Garante que a pasta de logs exista
